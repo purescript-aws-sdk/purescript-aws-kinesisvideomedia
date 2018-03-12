d10f65cb254fa4da0a70e14afb963c2e2b2a0016
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:04:47 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/KinesisVideoMedia.purs
+++ src/KinesisVideoMedia.purs
@@ -20,12 +20,12 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "KinesisVideoMedia" :: String
-
 
 -- | <p> Use this API to retrieve media content from a Kinesis video stream. In the request, you identify stream name or stream Amazon Resource Name (ARN), and the starting chunk. Kinesis Video Streams then returns a stream of chunks in order by fragment number.</p> <note> <p> You must first call the <code>GetDataEndpoint</code> API to get an endpoint to which you can then send the <code>GetMedia</code> requests. </p> </note> <p>When you put media data (fragments) on a stream, Kinesis Video Streams stores each incoming fragment and related metadata in what is called a "chunk." For more information, see . The <code>GetMedia</code> API returns a stream of these chunks starting from the chunk that you specify in the request. </p> <p>The following limits apply when using the <code>GetMedia</code> API:</p> <ul> <li> <p>A client can call <code>GetMedia</code> up to five times per second per stream. </p> </li> <li> <p>Kinesis Video Streams sends media data at a rate of up to 25 megabytes per second (or 200 megabits per second) during a <code>GetMedia</code> session. </p> </li> </ul>
 getMedia :: forall eff. GetMediaInput -> Aff (exception :: EXCEPTION | eff) GetMediaOutput
-getMedia = Request.request serviceName "getMedia" 
+getMedia = Request.request service method  where
+    service = Request.ServiceName "KinesisVideoMedia"
+    method = Request.MethodName "getMedia"
 
 
 -- | <p>Kinesis Video Streams has throttled the request because you have exceeded the limit of allowed client calls. Try making the call later.</p>
